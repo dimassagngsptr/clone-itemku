0e5814c29a191d119bd10ee2714e51bc387da618
dimassagngsptr <dimasageng58@gmail.com>
2024-05-04 09:18:36 +0700

    fix: card

--- src/components/base/Card/index.jsx
+++ src/components/base/Card/index.jsx
@@ -1,6 +1,6 @@
 const Card = ({ children }) => {
   return (
-    <div className="border border-gray-300 rounded-md h-[280px] w-[110px] lg:w-[150px] lg:h-[230px]">
+    <div className="border border-gray-300 rounded-md h-[280px] w-[110px] lg:w-[150px] lg:h-[250px]">
       {children}
     </div>
   );
